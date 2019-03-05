630dfd0faaff61adf10640701cbd5ed0b886760e
GregGaoter <greg.gautier@hotmail.com>
2019-03-05 13:22:26 +0100

    Correction d'un bug dans le calcul de la réponse du Mastermind.

--- src/main/java/com/openclassrooms/jeuxlogiques/controleur/service/ServiceDeCalculMastermind.java
+++ src/main/java/com/openclassrooms/jeuxlogiques/controleur/service/ServiceDeCalculMastermind.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.jeuxlogiques.controleur.service;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,20 +10,34 @@ import com.openclassrooms.jeuxlogiques.modele.enumeration.PionReponseCouleur;
 
 public class ServiceDeCalculMastermind implements ServiceDeCalcul {
 
-	// TODO Corriger le bug du calcul de la réponse de Mastermind
 	public List<Pion> calculerReponse(List<Pion> proposition, List<Pion> solution) {
 
 		List<Pion> reponse = new ArrayList<>(proposition.size());
 
+		/*
+		 * Récupère les valeurs des listes proposition et réponse
+		 */
+		List<Integer> listeValeursProposition = new ArrayList<>(proposition.size());
+		List<Integer> listeValeursSolution = new ArrayList<>(solution.size());
+		Iterator<Pion> itProposition = proposition.iterator();
+		Iterator<Pion> itSolution = solution.iterator();
+		while (itProposition.hasNext() && itSolution.hasNext()) {
+			listeValeursProposition.add(itProposition.next().getValeur());
+			listeValeursSolution.add(itSolution.next().getValeur());
+		}
+
+		/*
+		 * Compte le nombre de pions bien placés
+		 */
 		int nbBienPlace = 0;
 		List<Integer> positionBienPlace = new ArrayList<>();
 		int position = -1;
-		Iterator<Pion> itProposition = proposition.iterator();
-		Iterator<Pion> itSolution = solution.iterator();
+		Iterator<Integer> itValeursProposition = listeValeursProposition.iterator();
+		Iterator<Integer> itValeursSolution = listeValeursSolution.iterator();
 		int valeurProposition, valeurSolution;
-		while (itProposition.hasNext() && itSolution.hasNext()) {
-			valeurProposition = itProposition.next().getValeur();
-			valeurSolution = itSolution.next().getValeur();
+		while (itValeursProposition.hasNext() && itValeursSolution.hasNext()) {
+			valeurProposition = itValeursProposition.next();
+			valeurSolution = itValeursSolution.next();
 			position++;
 			if (valeurProposition == valeurSolution) {
 				nbBienPlace++;
@@ -32,21 +45,25 @@ public class ServiceDeCalculMastermind implements ServiceDeCalcul {
 			}
 		}
 
-		List<Pion> propositionTemp = new ArrayList<>(proposition.size());
-		List<Pion> solutionTemp = new ArrayList<>(solution.size());
-		propositionTemp.addAll(proposition);
-		solutionTemp.addAll(solution);
+		/*
+		 * Supprime les pions bien placés des listes proposition et solution
+		 */
 		for (int i = positionBienPlace.size() - 1; i >= 0; i--) {
-			propositionTemp.remove((int) positionBienPlace.get(i));
-			solutionTemp.remove((int) positionBienPlace.get(i));
+			listeValeursProposition.remove((int) positionBienPlace.get(i));
+			listeValeursSolution.remove((int) positionBienPlace.get(i));
 		}
 
-		propositionTemp.retainAll(solutionTemp);
-		HashSet<Pion> pionsMalPlaces = new HashSet<>();
-		pionsMalPlaces.addAll(propositionTemp);
+		/*
+		 * Compte le nombre de pions mal placés
+		 */
+		int nbMalPlace = 0;
+		itValeursProposition = listeValeursProposition.iterator();
+		while (itValeursProposition.hasNext())
+			nbMalPlace += listeValeursSolution.remove(itValeursProposition.next()) ? 1 : 0;
 
-		int nbMalPlace = pionsMalPlaces.size();
-
+		/*
+		 * Construction de la réponse
+		 */
 		for (int i = 0; i < nbBienPlace; i++)
 			reponse.add(PionReponseCouleur.Noir);
 		for (int i = 0; i < nbMalPlace; i++)
